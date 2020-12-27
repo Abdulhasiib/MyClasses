727adf96c8cc6b5b5c7cdd8fed1febee5534dd05
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-12-27 19:33:49 +0530

    Added Test for testing object types

--- MyClassesTest/AssertClassTest.cs
+++ MyClassesTest/AssertClassTest.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
 
 namespace MyClassesTest
 {
@@ -36,6 +37,24 @@ namespace MyClassesTest
             string str2 = "abcd";
 
             Assert.AreNotEqual(str1, str2);
+        }
+
+        [TestMethod]
+        public void AreSame()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = x;
+
+            Assert.AreSame(x, y);
+        }
+
+        [TestMethod]
+        public void AreNotSame()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = new FileProcess();
+
+            Assert.AreNotSame(x, y);
         }
     }
 }
